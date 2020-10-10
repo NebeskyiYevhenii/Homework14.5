7586d98a23761264677752402f885c2aa8ba8612
NebeskyiYevhenii <evheniy.slav@gmail.com>
2020-10-10 16:28:45 +0300

    end

---
+++ Homework14.5/LinkedList.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homework14._5
+{
+
+    public class Node
+    {
+        public string Name { get; set; }
+        public Node Next { get; set; }
+        public Node(string name)
+        {
+            Name = name;
+        }
+    }
+    public class LinkedList<T> : IEnumerable<string>
+    {
+        Node head;
+        Node tail;
+        int count;
+
+        public delegate void DeleteAndAddToNewLinkedList(string name);
+
+        public event DeleteAndAddToNewLinkedList DeleteNode;
+        public void OnDeleteItem(string name)
+        {
+            DeleteNode?.Invoke(name);
+        }
+
+        public void AddByIndex(string name, int index)
+        {
+            Node newNode = new Node(name);
+            Node node = head;
+            Node previous = null;
+            int i = 1;
+
+            while (i <= count)
+            {
+                if (i == index)
+                {
+                    previous.Next = newNode;
+                    newNode.Next = node;
+                    break;
+                }
+                previous = node;
+                node = node.Next;
+                i++;
+            }
+            count++;
+        }
+
+        public void AddToEnd(string name)
+        {
+            Node node = new Node(name);
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+                tail.Next = node;
+            tail = node;
+
+            count++;
+        }
+
+        public void AddToStart(string name)
+        {
+            Node node = new Node(name);
+
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                node.Next = head;
+                head = node;
+                count++;
+            }
+        }
+
+        public bool Delete(string name)
+        {
+            DeleteAndAddToNewLinkedList deleteAndAddToNewLinkedList = AddToEnd;
+
+            Node item = head;
+            Node previous = null;
+            bool Return = false;
+            if (head != null)
+            {
+                while (item != null)
+                {
+                    if (item.Name.Equals(name))
+                    {
+                        previous.Next = item.Next;
+                        count--;
+                        Return = true;
+                        OnDeleteItem(name);
+                    }
+                    previous = item;
+                    item = item.Next;
+                }
+                return Return;
+            }
+            return false;
+        }
+
+        public LinkedList<T> Filter(Node node)
+        {
+            LinkedList<T> LL = new LinkedList<T>();
+            Node item = head;
+            Node previous = null;
+
+            if (head != null)
+            {
+                while (item != null)
+                {
+                    if (item.Name.Equals(node.Name))
+                    {
+                        previous.Next = item.Next;
+                        count--;
+                    }
+                    previous = item;
+                    item = item.Next;
+                }
+                return LL;
+            }
+            return LL;
+        }
+
+        public Node this[int index]
+        {
+            set
+            {
+                Node node = head;
+                int i = 1;
+
+                while (i <= index)
+                {
+                    if (i == index)
+                    {
+                        node = value;
+                    }
+                    node = node.Next;
+                    i++;
+                }
+            }
+            get
+            {
+
+                Node indexNode = head;
+                Node node = head;
+                Node previous = null;
+                int i = 1;
+
+                while (i <= index)
+                {
+                    if (i == index)
+                    {
+                        return node;
+                    }
+                    previous = node;
+                    node = node.Next;
+                    i++;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                yield return current.Name;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+    }
+
+}
---
+++ Homework14.5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList1 = new LinkedList<string>();
+            LinkedList<string> linkedList2 = new LinkedList<string>();
+            linkedList1.DeleteNode += linkedList2.AddToStart;
+            // добавление элементов
+            linkedList1.AddToEnd("Саша");
+            linkedList1.AddToEnd("Маша");
+            linkedList1.AddToEnd("Даша");
+            linkedList1.AddToEnd("Даша");
+            linkedList1.AddToEnd("Глаша");
+
+            //var i = linkedList1.Where(x => x == "Даша");
+
+            // выводим элементы
+            foreach (var item in linkedList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // удаляем элемент
+            linkedList1.Delete("Маша");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("Delete \"Маша\"");
+            foreach (var item in linkedList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // проверяем наличие элемента
+            bool isPresent = linkedList1.Contains("Дима");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("проверяем наличие элемента \"Дима\"");
+            Console.WriteLine(isPresent == true ? "Дима присутствует" : "Sam отсутствует");
+
+            // проверяем наличие элемента
+            isPresent = linkedList1.Contains("Глаша");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("проверяем наличие элемента \"Глаша\"");
+            Console.WriteLine(isPresent == true ? "Глаша присутствует" : "Sam отсутствует");
+
+            // добавляем элемент в начало            
+            linkedList1.AddToStart("Сергей");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в начало  \"Сергей\"");
+            foreach (var item in linkedList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // добавляем элемент в конец            
+            linkedList1.AddToEnd("Яна");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в конец  \"Яна\"");
+            foreach (var item in linkedList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // добавляем элемент в индекс 3            
+            linkedList1.AddByIndex("Миша", 3);
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("добавляем элемент в индекс 3  \"Миша\"");
+            foreach (var item in linkedList1)
+            {
+                Console.WriteLine(item);
+            }
+
+            // получаем 3 элемент
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("получаем 3 элемент");
+            Console.WriteLine(linkedList1[3].Name);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
